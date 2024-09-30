091132800355b36ce75f7bc44af8f91f12737c89
Pawel-Raciborski <wojak2845@gmail.com>
2024-09-30 17:23:09 +0200

    Backend project initialization

---
+++ Projekt/Backend/system-kliniki-medycznej/src/test/java/org/back/systemklinikimedycznej/MedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.back.systemklinikimedycznej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
