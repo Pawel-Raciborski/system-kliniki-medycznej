d491745d1018d042f603252fa831a0016b46b442
Pawel-Raciborski <wojak2845@gmail.com>
2024-11-11 19:21:53 +0100

    Dodanie globalnego ExceptionHandlera do obsługiwania błędów w aplikacji

--- Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/exceptions/GlobalAppException.java
+++ Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/exceptions/GlobalAppException.java
@@ -1,7 +1,9 @@
 package org.back.systemklinikimedycznej.exceptions;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class GlobalAppException extends RuntimeException {
     private final String message;
     private final HttpStatus status;
