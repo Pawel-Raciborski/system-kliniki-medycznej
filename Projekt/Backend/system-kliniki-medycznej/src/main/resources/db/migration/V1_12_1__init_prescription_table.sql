489a547fa5b3b11668fb9c82312eeb5ab4aa43c5
Pawel-Raciborski <wojak2845@gmail.com>
2024-11-11 19:22:51 +0100

    Modyfikacja migracji: - zmiana typu pola `id` na UUID

--- Projekt/Backend/system-kliniki-medycznej/src/main/resources/db/migration/V1_12_1__init_prescription_table.sql
+++ Projekt/Backend/system-kliniki-medycznej/src/main/resources/db/migration/V1_12_1__init_prescription_table.sql
@@ -1,6 +1,6 @@
 create table prescription
 (
-    id              SERIAL NOT NULL PRIMARY KEY,
+    id              uuid NOT NULL PRIMARY KEY,
     doctor_id       INT    NOT NULL,
     patient_id      INT    NOT NULL,
     expiration_date date   NOT NULL,
