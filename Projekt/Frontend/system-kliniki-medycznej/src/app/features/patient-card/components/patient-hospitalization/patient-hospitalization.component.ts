525ba18ff61978bf7bafc896787ab3b074e8faf3
Pawel-Raciborski <wojak2845@gmail.com>
2024-11-13 14:54:41 +0100

    Utworzenie komponentu wyświetlającego listę informacji nt. hospitalizacji pacjenta

---
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/patient-card/components/patient-hospitalization/patient-hospitalization.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-hospitalization',
+  standalone: true,
+  imports: [],
+  templateUrl: './patient-hospitalization.component.html',
+  styleUrl: './patient-hospitalization.component.css'
+})
+export class PatientHospitalizationComponent {
+
+}
