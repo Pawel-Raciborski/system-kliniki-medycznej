8ad9a0f6d61216a1a9b670f1554524a58a2ed82f
Pawel-Raciborski <wojak2845@gmail.com>
2024-11-14 23:32:44 +0100

    Modyfikacja card-summary

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/patient-card/components/card-summary/card-summary.component.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/patient-card/components/card-summary/card-summary.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {PatientCardService} from '../../services/patient-card.service';
 
 @Component({
   selector: 'app-card-summary',
@@ -10,6 +11,17 @@ import {RouterLink} from '@angular/router';
   templateUrl: './card-summary.component.html',
   styleUrl: './card-summary.component.css'
 })
-export class CardSummaryComponent {
+export class CardSummaryComponent implements OnInit{
+  @Input({required:true}) patientCardId!: string;
 
+  constructor(private patientCardService: PatientCardService) {
+  }
+
+  ngOnInit(): void {
+        this.patientCardService.getCardSummary(this.patientCardId).subscribe(
+          data => {
+            console.log(data);
+          }
+        );
+    }
 }
