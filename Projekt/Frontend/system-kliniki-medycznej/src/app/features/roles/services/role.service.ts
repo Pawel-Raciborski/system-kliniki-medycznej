4e0bbc8ab113d22c1ae0f944685604db6b25bb27
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-02 19:35:25 +0100

    Dodanie nowych metod w serwisie

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/roles/services/role.service.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/roles/services/role.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {Role} from '../domain/role';
+import {RoleDetails} from '../domain/role-details';
+import {Permission} from '../../permission/model/permission';
 
 @Injectable({
   providedIn: 'root'
@@ -23,8 +25,68 @@ export class RoleService {
 
   private createRole(role: string, username: string) : Role{
     return {
+      id: 1,
       name: role,
       description: (username + role),
     };
+  }
+
+  findAll() : Observable<Role[]>{
+    return of([
+      {
+        id: 1,
+        name: "DOCTOR",
+        description: "DOCTOR",
+      },
+      {
+        id: 2,
+        name: "RECEPTIONIST",
+        description: "RECEPTIONIST",
+      }
+      ,
+      {
+        id: 3,
+        name: "PATIENT",
+        description: "PATIENT",
+      }
+    ]);
+
+    //"RECEPTIONIST",
+    //         "PATIENT",
+    //         "GUEST_PATIENT"
+  }
+
+  remove(role: Role) : Observable<Role>{
+    return of(role);
+  }
+
+  getRoleDetails(roleId: number): Observable<RoleDetails> {
+    return of({
+      role: {
+        id: 1,
+        name: "DOCTOR",
+        description: "description",
+      },
+      permissions: [
+        {
+          id: 12,
+          name: "add_cos",
+          description: "opis"
+        },
+        {
+          id: 15,
+          name: "remove_calendar",
+          description: "opis"
+        }
+      ]
+    })
+  }
+
+  removePermissionFromRole(roleId: Role, permission: Permission) : Observable<Permission> {
+    return of(permission);
+  }
+
+  addPermissionToRole(role: Role, permissionsToAdd: Permission[]) : Observable<Permission[]> {
+    return of(permissionsToAdd);
   }
 }
