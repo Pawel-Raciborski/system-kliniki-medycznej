b4d0400a2305ddfb9c8014c6536bcdfbd442a041
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-02 17:23:22 +0100

    Inicjalizacja podstawowych komponentów zarządzania rolami

---
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/roles/components/roles-table/roles-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles-table.component.html',
+  styleUrl: './roles-table.component.css'
+})
+export class RolesTableComponent {
+
+}
