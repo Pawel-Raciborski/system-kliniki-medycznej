9a04ad58554cd7a560e664ed89ece1a997d35d4f
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-04 17:31:22 +0100

    - Dodano elementy do widoku .html recept lekarza - Dodano logikę do pobierania recept oraz wyszukiwania recept pacjenta

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/prescriptions/components/doctor-prescriptions/doctor-prescriptions.component.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/prescriptions/components/doctor-prescriptions/doctor-prescriptions.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PrescriptionsTableComponent} from '../prescriptions-table/prescriptions-table.component';
+import {TableOptionsComponent} from '../../../doctor/components/doctor-table/table-options/table-options.component';
+import {Pagination} from '../../../pagination/model/pagination';
+import {PrescriptionInfo} from '../../model/prescription-info';
+import {PrescriptionService} from '../../services/prescription.service';
+import {UserService} from '../../../auth/services/user.service';
+import {PatientSearchBarComponent} from '../../../patient/components/patient-search-bar/patient-search-bar.component';
+import {SearchPatient} from '../../../patient/model/search-patient';
 
 @Component({
   selector: 'app-doctor-prescriptions',
   standalone: true,
-  imports: [],
+  imports: [
+    PrescriptionsTableComponent,
+    TableOptionsComponent,
+    PatientSearchBarComponent
+  ],
   templateUrl: './doctor-prescriptions.component.html',
   styleUrl: './doctor-prescriptions.component.css'
 })
-export class DoctorPrescriptionsComponent {
+export class DoctorPrescriptionsComponent implements OnInit {
+  paginationOptions: Pagination = {
+    page: 0,
+    pageSize: 10
+  };
+  lastLoadedPageSize = this.paginationOptions.pageSize;
+  prescriptions: PrescriptionInfo[] = [];
 
+  constructor(
+    private prescriptionService: PrescriptionService,
+    private userService: UserService,
+  ) {
+  }
+
+  ngOnInit(): void {
+        this.loadMore();
+  }
+
+  loadMore() {
+    this.prescriptionService.getDoctorPrescriptions(
+      this.userService.getId("doctorId"),
+      this.paginationOptions
+    ).subscribe(data => {
+        this.prescriptions = [...this.prescriptions,...data];
+        this.lastLoadedPageSize = data.length;
+    });
+  }
+
+  searchPrescriptionsForPatient(searchPatient: SearchPatient) {
+    this.clearPagination();
+    this.prescriptionService.findAllPrescriptionsForPatient(searchPatient)
+      .subscribe(data => {
+        this.prescriptions = data;
+      })
+  }
+
+  private clearPagination() {
+    this.paginationOptions.page = 0;
+    this.paginationOptions.pageSize = 10;
+  }
 }
