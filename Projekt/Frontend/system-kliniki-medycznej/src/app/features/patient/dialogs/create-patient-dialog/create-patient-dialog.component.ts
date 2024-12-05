22206e66faeb1ef2351e4e67002905ccb74a2e0c
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-05 19:38:45 +0100

    Utworzenie komponentu dodawania użytkownika

---
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/patient/dialogs/create-patient-dialog/create-patient-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-patient-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-patient-dialog.component.html',
+  styleUrl: './create-patient-dialog.component.css'
+})
+export class CreatePatientDialogComponent {
+
+}
