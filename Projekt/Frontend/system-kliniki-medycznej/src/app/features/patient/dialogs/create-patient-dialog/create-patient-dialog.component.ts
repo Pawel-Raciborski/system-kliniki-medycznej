c2beb9f5569a8d47e0e213f4cb7e15970a094564
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-05 21:42:15 +0100

    - dodanie logiki do tworzenia pacjenta

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/patient/dialogs/create-patient-dialog/create-patient-dialog.component.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/patient/dialogs/create-patient-dialog/create-patient-dialog.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-patient-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './create-patient-dialog.component.html',
   styleUrl: './create-patient-dialog.component.css'
 })
 export class CreatePatientDialogComponent {
+  form = new FormGroup({
+    pesel : new FormControl(''),
+    parentPesel : new FormControl(''),
+    name : new FormControl(''),
+    surname : new FormControl(''),
+    birthDate : new FormControl(''),
+    gender : new FormControl(''),
+    phoneNumber : new FormControl(''),
+    address : new FormGroup({
+      street : new FormControl(''),
+      apartmentNumber : new FormControl(''),
+      postalCode : new FormControl(''),
+      city : new FormControl(''),
+    })
+  });
 
+  constructor(
+    private dialogRef: MatDialogRef<CreatePatientDialogComponent>
+  ) {
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
+
+  addPatient() {
+    this.dialogRef.close({...this.form.value});
+  }
 }
