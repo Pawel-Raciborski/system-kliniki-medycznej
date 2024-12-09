27dea2e9b4d9c9b5b994055b23191280e7f4656d
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-09 23:37:49 +0100

    Utworzenie menu recepcjonisty

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/receptionist-panel/components/receptionist-menu/receptionist-menu.component.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/receptionist-panel/components/receptionist-menu/receptionist-menu.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {UserService} from '../../../auth/services/user.service';
 
 @Component({
   selector: 'app-receptionist-menu',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './receptionist-menu.component.html',
   styleUrl: './receptionist-menu.component.css'
 })
-export class ReceptionistMenuComponent {
+export class ReceptionistMenuComponent implements OnInit{
+  receptionistId!: number;
+
+  constructor(
+    private userService: UserService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.receptionistId = this.userService.getId("receptionistId");
+  }
 
 }
