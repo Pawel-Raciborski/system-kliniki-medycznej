ba3afd3ac0e76cee654bf6d368b1da736ec76718
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-12 16:12:58 +0100

    Utworzenie formularza tworzenia konta pacjenta

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/account/dialogs/create-patient-account-dialog/create-patient-account-dialog.component.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/account/dialogs/create-patient-account-dialog/create-patient-account-dialog.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-create-patient-account-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './create-patient-account-dialog.component.html',
   styleUrl: './create-patient-account-dialog.component.css'
 })
 export class CreatePatientAccountDialogComponent {
-
+  form: FormGroup = new FormGroup({
+    pesel: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    email: new FormControl(''),
+  });
 }
