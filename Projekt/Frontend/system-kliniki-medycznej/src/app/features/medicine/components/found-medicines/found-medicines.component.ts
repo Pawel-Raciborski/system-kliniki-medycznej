9b20f238e50618fb4f94b480ef8ab6237b5b2b39
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-12 19:24:40 +0100

    Modyfikacja komponentu znalezionych leków

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/medicine/components/found-medicines/found-medicines.component.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/medicine/components/found-medicines/found-medicines.component.ts
@@ -1,5 +1,15 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {MedicineDto} from '../../model/medicine-dto';
+import {PrescriptionMedicine} from '../../../prescriptions/model/prescription-medicine';
+import {MatDialog} from '@angular/material/dialog';
+import {MedicineDosageDialogComponent} from '../../dialogs/medicine-dosage-dialog/medicine-dosage-dialog.component';
+import {HospitalizationInfo} from '../../../patient-card/model/hospitalization-info';
+import {
+  AddHospitalizationDialogComponent
+} from '../../../patient-card/dialogs/add-hospitalization-dialog/add-hospitalization-dialog.component';
+import {
+  CreateNewHospitalizationDialogComponent
+} from '../../../patient-card/dialogs/create-new-hospitalization-dialog/create-new-hospitalization-dialog.component';
 
 @Component({
   selector: 'app-found-medicines',
@@ -10,14 +20,34 @@ import {MedicineDto} from '../../model/medicine-dto';
 })
 export class FoundMedicinesComponent {
   @Input() medicines: MedicineDto[] = [];
-  @Output() medicineToAddEmitter = new EventEmitter<MedicineDto>();
+  @Input({required: true}) type!: "prescriptionMedicine" | "hospitalization" | "medicinePanel";
+  @Output() medicineToAddEmitter = new EventEmitter<any>();
 
-  addMedicine(medicine: MedicineDto) {
-    this.removeMedicine(medicine);
-    this.medicineToAddEmitter.emit(medicine);
+  constructor(
+    private dialog: MatDialog
+  ) {
   }
 
-  private removeMedicine(medicine: MedicineDto) {
-    this.medicines = this.medicines.filter(m => m.id !== medicine.id);
+  addMedicine(medicine: MedicineDto) {
+    if (this.type === 'prescriptionMedicine') {
+      this.dialog.open(MedicineDosageDialogComponent, {
+        data: medicine
+      }).afterClosed().subscribe((prescriptionMedicine: PrescriptionMedicine) => {
+        console.log(prescriptionMedicine);
+        if (prescriptionMedicine) {
+          this.medicineToAddEmitter.emit(prescriptionMedicine);
+        }
+      });
+    } else if (this.type === 'hospitalization') {
+      this.dialog.open(CreateNewHospitalizationDialogComponent, {
+        data: medicine
+      }).afterClosed().subscribe((prescriptionMedicine: { medicine: MedicineDto; dosage: string; notes: string, finishDate:string }) => {
+        console.log(prescriptionMedicine);
+        if (prescriptionMedicine) {
+          this.medicineToAddEmitter.emit(prescriptionMedicine);
+        }
+      });
+    }
+
   }
 }
