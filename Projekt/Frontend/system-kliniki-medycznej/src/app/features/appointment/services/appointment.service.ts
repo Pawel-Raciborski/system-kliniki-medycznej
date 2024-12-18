725b5b4b662c15126b16ad54ee04945e8e6e3c95
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-18 22:50:37 +0100

    - Dodanie httpClient oraz połączenia do backendu

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/appointment/services/appointment.service.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/appointment/services/appointment.service.ts
@@ -1,77 +1,24 @@
 import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 import {PatientAppointmentInfo} from '../model/patient-appointment-info';
 import {AppointmentDetails} from '../model/appointment-details';
+import {FinishAppointmentRequest} from '../model/finish-appointment-request';
+import {AppointmentInfo} from '../model/appointment-info';
+import {CalendarAppointmentsResponse} from '../../doctor/domain/calendar/calendar-appointments-response';
+import {environment} from '../../../../environments/environment.dev';
+import {HttpClient, HttpParams, HttpResponse} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
-  arr = [
-    {
-      id: "c0a8012c-7e6b-4e8f-900e-2e003d00c001",
-      doctorName: "Emily",
-      doctorSurname: "Jones",
-      appointmentDate: "2024-11-12T14:30:00Z",
-      appointmentStatus: "Pending"
-    },
-    {
-      id: "c0a8012c-7e6b-4e8f-900e-2e003d00c002",
-      doctorName: "Michael",
-      doctorSurname: "Smith",
-      appointmentDate: "2024-11-13T09:00:00Z",
-      appointmentStatus: "Confirmed"
-    },
-    {
-      id: "c0a8012c-7e6b-4e8f-900e-2e003d00c003",
-      doctorName: "Sarah",
-      doctorSurname: "Brown",
-      appointmentDate: "2024-11-14T11:15:00Z",
-      appointmentStatus: "Canceled"
-    },
-    {
-      id: "c0a8012c-7e6b-4e8f-900e-2e003d00c004",
-      doctorName: "John",
-      doctorSurname: "Davis",
-      appointmentDate: "2024-11-15T16:45:00Z",
-      appointmentStatus: "Pending"
-    },
-    {
-      id: "c0a8012c-7e6b-4e8f-900e-2e003d00c005",
-      doctorName: "Jessica",
-      doctorSurname: "Wilson",
-      appointmentDate: "2024-11-16T13:30:00Z",
-      appointmentStatus: "Confirmed"
-    },
-    {
-      id: "c0a8012c-7e6b-4e8f-900e-2e003d00c006",
-      doctorName: "Robert",
-      doctorSurname: "Martinez",
-      appointmentDate: "2024-11-17T10:00:00Z",
-      appointmentStatus: "Pending"
-    },
-    {
-      id: "c0a8012c-7e6b-4e8f-900e-2e003d00c007",
-      doctorName: "Laura",
-      doctorSurname: "Garcia",
-      appointmentDate: "2024-11-18T15:45:00Z",
-      appointmentStatus: "Completed"
-    },
-  ];
+  url = `${environment.serverUrl}/appointments`
+  patientAppointmentsUrl = `${environment.serverUrl}/patient-appointments`;
 
-  constructor() {
-  }
 
-  findPatientNextAppointment(patientId: string): Observable<PatientAppointmentInfo> {
-    let patientAppointmentInfo: PatientAppointmentInfo = {
-      id: "c0a8012c-7e6b-4e8f-900e-2e003d00c000",
-      doctorName: "Emily",
-      doctorSurname: "Jones",
-      appointmentDate: "2024-11-12T14:30:00Z",
-      appointmentStatus: "Pending"
-    };
-
-    return of(patientAppointmentInfo);
+  constructor(
+    private httpClient: HttpClient
+  ) {
   }
 
   createAppointment(appointmentDateTime: {
@@ -79,59 +26,95 @@ export class AppointmentService {
     date: string,
     hour: string,
     doctorPwzNumber: string
-  }) {
-    console.log(appointmentDateTime);
+  }): Observable<AppointmentInfo> {
+    return this.httpClient.post<AppointmentInfo>(
+      `${this.url}/schedule-appointment`,
+      appointmentDateTime
+    );
   }
 
   findPatientUpcomingAppointments(patientId: number, pageProperties: {
     page: number,
     pageSize: number
   }): Observable<PatientAppointmentInfo[]> {
-    console.log('PATIENTID:', patientId, pageProperties);
-    let slice = this.arr.slice(pageProperties.page * pageProperties.pageSize, (pageProperties.page + 1) * pageProperties.pageSize);
-    console.log(`SLICE: ${slice.length}`);
-    return of(slice);
+    return this.httpClient.get<PatientAppointmentInfo[]>(
+      `${this.patientAppointmentsUrl}`,
+      {
+        params: {
+          page: pageProperties.page,
+          pageSize: pageProperties.pageSize,
+          patientId: patientId
+        }
+      }
+    );
   }
 
   findPatientFinishedAppointments(patientId: number, pageProperties: {
     page: number;
     pageSize: number
   }): Observable<PatientAppointmentInfo[]> {
-    console.log(patientId);
-    let slice = this.arr.slice(pageProperties.page * pageProperties.pageSize, (pageProperties.page + 1) * pageProperties.pageSize);
-    console.log(`SLICE: ${slice.length}`);
-    return of(slice);
+    return this.httpClient.get<PatientAppointmentInfo[]>(
+      `${this.patientAppointmentsUrl}/finished`,
+      {
+        params: {
+          page: pageProperties.page,
+          pageSize: pageProperties.pageSize,
+          patientId: patientId
+        }
+      }
+    );
   }
 
   getAppointmentDetails(appointmentId: string): Observable<AppointmentDetails> {
-    const appDetails: AppointmentDetails = {
-      appointment: {
-        id: "ddd582b7-1f39-4118-b9e0-6ae1dc8e2d41",
-        date: "2024-11-18",
-        hour: "09:30",
-        status: "Umówiono"
-      },
-      patientData: {
-        id: 2,
-        personalDetails: {
-          pesel: "01249381623",
-          name: "Piotr",
-          surname: "Piotrowski",
-          birthDate: "2001-04-20",
-          gender: "Mężczyzna",
-          phoneNumber: "+48 174 625 512",
-          address: {
-            street: "ul.Jana Pawła II",
-            apartmentNumber: "5",
-            postalCode: "12-492",
-            city: "Białystok"
-          }
-        },
-        parentPesel: "null"
-      },
-      patientCardId: "e7597d41-27b5-4a96-b529-6ff264bd4097"
-    };
+    return this.httpClient.get<AppointmentDetails>(
+      `${this.url}/${appointmentId}/details`
+    );
+  }
 
-    return of(appDetails);
+  finishAppointment(appointmentRequest: FinishAppointmentRequest) {
+    return this.httpClient.put<HttpResponse<void>>(
+      `${this.url}/finish`,
+      appointmentRequest
+    );
+  }
+
+  getAvailableStatuses(): Observable<{ value: string, name: string }[]> {
+    return this.httpClient.get<{ value: string, name: string }[]>(
+      `${this.url}/available-statuses`
+    );
+  }
+
+  updateStatus(appointmentId: string, newStatus: string): Observable<AppointmentInfo> {
+    return this.httpClient.put<AppointmentInfo>(
+      `${this.url}/update-status`,
+      {},
+      {
+        params: {
+          id: appointmentId,
+          newStatus: newStatus
+        }
+      }
+    )
+  }
+
+  findDoctorAppointments(formatType?: 'week' | 'month', id?: number, date?: string): Observable<CalendarAppointmentsResponse> {
+    let httpParams = new HttpParams();
+
+    if (formatType) {
+      httpParams = httpParams.append("formatType", formatType);
+    }
+    if (id) {
+      httpParams = httpParams.append("doctorId", id);
+    }
+    if (date) {
+      httpParams = httpParams.append("date", date);
+    }
+    console.log(httpParams);
+    return this.httpClient.get<CalendarAppointmentsResponse>(
+      `${environment.serverUrl}/doctors/calendar/all-appointments`,
+      {
+        params: httpParams
+      }
+    );
   }
 }
