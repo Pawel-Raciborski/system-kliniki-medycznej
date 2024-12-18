593a3886aeed7e6cc7db631684b24ea28e1eac26
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-18 22:30:00 +0100

    Dodanie pola doctor

--- Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/appointment/controllers/dto/AppointmentDetails.java
+++ Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/appointment/controllers/dto/AppointmentDetails.java
@@ -1,6 +1,7 @@
 package org.back.systemklinikimedycznej.appointment.controllers.dto;
 
 import lombok.Builder;
+import org.back.systemklinikimedycznej.doctor.controller.dto.DoctorInfo;
 import org.back.systemklinikimedycznej.patient.controllers.dto.PatientData;
 import org.back.systemklinikimedycznej.patient.repositories.entities.patient_card.PatientCard;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 public record AppointmentDetails(
         AppointmentInfo appointment,
         PatientData patientData,
+        DoctorInfo doctor,
         UUID patientCardId
 ) {
 }
