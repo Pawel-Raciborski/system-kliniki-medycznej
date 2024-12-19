0ab657f14e9dc379c016bbe3dd59b48d0b577e74
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-19 23:05:04 +0100

    Dodanie metody POST z wyszukiwaniem leków

--- Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/cure/controller/MedicineController.java
+++ Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/cure/controller/MedicineController.java
@@ -1,10 +1,13 @@
-package org.back.systemklinikimedycznej.cure;
+package org.back.systemklinikimedycznej.cure.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.back.systemklinikimedycznej.cure.dto.MedicineDto;
 import org.back.systemklinikimedycznej.cure.dto.MedicineListDto;
+import org.back.systemklinikimedycznej.cure.dto.SearchMedicine;
+import org.back.systemklinikimedycznej.cure.mapper.MedicineMapper;
 import org.back.systemklinikimedycznej.cure.service.MedicineService;
 import org.back.systemklinikimedycznej.cure.web_client.MedicineWebClient;
+import org.back.systemklinikimedycznej.model.Pagination;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,5 +31,15 @@ public class MedicineController {
         return ResponseEntity.ok(listDto);
     }
 
-    @PostMapping()
+    @PostMapping("/search")
+    public ResponseEntity<List<MedicineDto>> searchMedicines(
+            @RequestParam("page") Integer page,
+            @RequestParam("pageSize") Integer pageSize,
+            @RequestBody SearchMedicine searchMedicine
+            ){
+        Pagination pagination = new Pagination(page,pageSize);
+        List<MedicineDto> medicines = medicineService.searchMedicine(searchMedicine,pagination);
+
+        return ResponseEntity.ok(medicines);
+    }
 }
