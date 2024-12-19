56f370f4f54768d5195aa160073ce0d6fd297231
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-19 23:09:00 +0100

    Utworzenie logiki wyszukiwania lekarzy

--- Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/doctor/services/DoctorService.java
+++ Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/doctor/services/DoctorService.java
@@ -1,11 +1,18 @@
 package org.back.systemklinikimedycznej.doctor.services;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.back.systemklinikimedycznej.account.repositories.entities.Account;
 import org.back.systemklinikimedycznej.account.services.AccountService;
+import org.back.systemklinikimedycznej.doctor.controller.dto.DoctorInfo;
 import org.back.systemklinikimedycznej.doctor.controller.dto.RegisterDoctorDetails;
+import org.back.systemklinikimedycznej.doctor.domain.AdvancedSearch;
+import org.back.systemklinikimedycznej.doctor.domain.DoctorSearchParams;
+import org.back.systemklinikimedycznej.doctor.domain.PrefixBuilder;
 import org.back.systemklinikimedycznej.doctor.exceptions.DoctorNotFoundException;
+import org.back.systemklinikimedycznej.doctor.mapper.DoctorMapper;
 import org.back.systemklinikimedycznej.doctor.repositories.DoctorRepository;
 import org.back.systemklinikimedycznej.doctor.repositories.entities.Doctor;
 import org.back.systemklinikimedycznej.doctor.repositories.entities.DoctorSpecialization;
@@ -22,8 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -37,6 +43,11 @@ public class DoctorService {
     private final DoctorSpecializationsService doctorSpecializationsService;
     private final AccountRoleService accountRoleService;
     private final DoctorFileService doctorFileService;
+    private final EntityManager entityManager;
+
+    public static boolean isNotEmptyAndBlank(String message) {
+        return Objects.nonNull(message) && !message.isBlank() && !message.isEmpty();
+    }
 
     @Transactional
     public Doctor create(RegisterDoctorDetails registerDoctorDetails, MultipartFile profileImage) {
@@ -91,16 +102,72 @@ public class DoctorService {
     }
 
     public Resource getFile(Doctor doctor, String fileName) {
-        return doctorFileService.findFile(doctor.getProfileImagePath(),fileName);
+        return doctorFileService.findFile(doctor.getProfileImagePath(), fileName);
     }
 
     public Doctor findById(Long id) {
         return doctorRepository.findById(id).orElseThrow(
-                () -> new DoctorNotFoundException("Nie znaleziono lekarza",HttpStatus.NOT_FOUND)
+                () -> new DoctorNotFoundException("Nie znaleziono lekarza", HttpStatus.NOT_FOUND)
         );
     }
 
     public Long countAvailableDoctors() {
         return doctorRepository.count();
+    }
+
+    public Doctor findByAccount(Account account) {
+        return doctorRepository.findByAccount(account)
+                .orElseThrow(
+                        () -> new DoctorNotFoundException("Nie znaleziono doktora!", HttpStatus.NOT_FOUND)
+                );
+    }
+
+    @Transactional
+    public List<DoctorInfo> searchForDoctors(DoctorSearchParams doctorSearchParams) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Doctor> query = criteriaBuilder.createQuery(Doctor.class);
+        Root<Doctor> doctorRoot = query.from(Doctor.class);
+        query.select(doctorRoot);
+        List<Predicate> predicates = new ArrayList<>();
+
+        String doctorFullName = doctorSearchParams.doctorFullName();
+        String[] doctorNameAndSurname = doctorFullName.trim().split(" ");
+        if (isNotEmptyAndBlank(doctorFullName)) {
+            String name = doctorNameAndSurname[0];
+            String surname = getSurname(doctorNameAndSurname);
+            Join<Doctor, PersonalDetails> personalDetails = doctorRoot.join("personalDetails");
+
+            if(isNotEmptyAndBlank(name)){
+                predicates.add(criteriaBuilder.like(personalDetails.get("name"), PrefixBuilder.startsWith(name)));
+            }
+            if(isNotEmptyAndBlank(surname)){
+
+                predicates.add(criteriaBuilder.like(personalDetails.get("surname"), PrefixBuilder.startsWith(surname)));
+            }
+        }
+
+        if (!Objects.isNull(doctorSearchParams.advancedSearch())) {
+            AdvancedSearch advancedSearch = doctorSearchParams.advancedSearch();
+            List<String> specializations = advancedSearch.selectedSpecializations();
+            String doctorPwzNumber = advancedSearch.pwzNumber();
+
+            if (Objects.nonNull(specializations) && !specializations.isEmpty()) {
+                Join<Doctor, DoctorSpecialization> doctorDoctorSpecializationJoin = doctorRoot.join("doctorSpecializations");
+                predicates.add(doctorDoctorSpecializationJoin.get("name").in(specializations));
+            }
+
+            if (isNotEmptyAndBlank(doctorPwzNumber)) {
+                predicates.add(criteriaBuilder.like(doctorRoot.get("pwzNumber"),PrefixBuilder.startsWith(doctorPwzNumber)));
+            }
+        }
+
+        query.where(predicates.toArray(new Predicate[0]));
+        return entityManager.createQuery(query).getResultList().stream()
+                .map(DoctorMapper.INSTANCE::mapToDoctorInfo)
+                .toList();
+    }
+
+    private static String getSurname(String[] doctorNameAndSurname) {
+        return doctorNameAndSurname.length == 2 ? doctorNameAndSurname[1] : "";
     }
 }
