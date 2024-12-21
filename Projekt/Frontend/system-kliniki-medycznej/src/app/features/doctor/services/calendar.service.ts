341ac9e43a28bc0ca8eb9c09d18e8edcf0cfbe2a
Pawel-Raciborski <wojak2845@gmail.com>
2024-12-21 22:10:01 +0100

    calendar service

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/doctor/services/calendar.service.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/doctor/services/calendar.service.ts
@@ -7,15 +7,23 @@ export class CalendarService {
 
   constructor() { }
 
-  setColor(status: "Umówiono" |
-  "Potwierdzono" |
-  "Utworzono" |
-  "W trakcie" |
-  "Zakończona" |
-  "Anulowana") {
+  setColor(status: string) {
 
     if(status === "Umówiono"){
       return "#f3de8a";
+    }
+    if(status === "Potwierdzono"){
+      return "#cae7b9";
+    }
+    if(status === "Zakończona"){
+      return "lightgray";
+    }
+    return "#eb9486";
+  }
+
+  setDarker(status: string) {
+    if(status === "Umówiono"){
+      return "#e2cd7a";
     }
     if(status === "Potwierdzono"){
       return "#cae7b9";
