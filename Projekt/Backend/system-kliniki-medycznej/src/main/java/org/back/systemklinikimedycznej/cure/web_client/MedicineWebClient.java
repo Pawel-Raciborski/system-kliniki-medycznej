3c66124bce61da32bee7ba115da5f6690531d0ee
Pawel-Raciborski <wojak2845@gmail.com>
2025-01-12 16:52:21 +0100

    Modyfikacja danych parametrów, dodanie metody searchMedicine

--- Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/cure/web_client/MedicineWebClient.java
+++ Projekt/Backend/system-kliniki-medycznej/src/main/java/org/back/systemklinikimedycznej/cure/web_client/MedicineWebClient.java
@@ -1,16 +1,13 @@
 package org.back.systemklinikimedycznej.cure.web_client;
 
 import org.back.systemklinikimedycznej.config.util.RPLApiEndpointUtil;
-import org.back.systemklinikimedycznej.cure.dto.MedicineDto;
 import org.back.systemklinikimedycznej.cure.dto.MedicineListDto;
-import org.back.systemklinikimedycznej.cure.repositories.entities.Medicine;
 import org.back.systemklinikimedycznej.cure.web_client.enums.SpecimenType;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
-import java.util.List;
 import java.util.Map;
 
 @Service
@@ -19,7 +16,7 @@ public class MedicineWebClient {
 
     public MedicineWebClient() {
         this.webClient = WebClient.builder()
-                .baseUrl(RPLApiEndpointUtil.MEDICINAL_PRODUCTS_SEARCH_ENDPOINT + "?specimenTypeEnum={specimenType}")
+                .baseUrl(RPLApiEndpointUtil.MEDICINAL_PRODUCTS_SEARCH_ENDPOINT + "?specimenTypeEnum={specimenType}&isAdvancedSearch=false")
                 .defaultUriVariables(Map.of(
                         "specimenType", SpecimenType.HUMAN.getName()
                 ))
@@ -28,11 +25,11 @@ public class MedicineWebClient {
 
     public MedicineListDto findByName(String name, int pageSize, int page, String sort){
         return this.webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/public")
+                .uri(uriBuilder -> uriBuilder
                         .queryParam("name",name)
                         .queryParam("specimenTypeEnum", SpecimenType.HUMAN.getName())
-                        .queryParam("subjectRolesIds",1)
-                        .queryParam("isAdvancedSearch",false)
+//                        .queryParam()
+//                        .queryParam()
                         .queryParam("size",pageSize)
                         .queryParam("page",page)
                         .queryParam("sort",sort).build())
@@ -46,7 +43,7 @@ public class MedicineWebClient {
 
     public MedicineListDto findByAtcCode(String atcCode) {
         return this.webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/public")
+                .uri(uriBuilder -> uriBuilder
                         .queryParam("atcCode",atcCode)
                         .queryParam("specimenTypeEnum", SpecimenType.HUMAN.getName())
                         .build()
@@ -63,9 +60,20 @@ public class MedicineWebClient {
 
     public MedicineListDto findByRegistryNumber(String registryNumber) {
         return this.webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/public")
+                .uri(uriBuilder -> uriBuilder
                         .queryParam("registryNumber",registryNumber)
                         .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(MedicineListDto.class)
+                .block();
+    }
+
+    public MedicineListDto searchMedicines(MultiValueMap<String, String> params) {
+        return this.webClient.get()
+                .uri(uri -> uri.queryParams(params)
+                        .build()
+                )
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(MedicineListDto.class)
