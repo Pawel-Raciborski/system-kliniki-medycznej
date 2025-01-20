606ff1f5e347ec0f534599d3a927cc0fbe8595ce
Pawel-Raciborski <wojak2845@gmail.com>
2025-01-20 09:51:49 +0100

    Modyfikacja serwisu

--- Projekt/Frontend/system-kliniki-medycznej/src/app/features/doctor-specialization/services/doctor-specialization.service.ts
+++ Projekt/Frontend/system-kliniki-medycznej/src/app/features/doctor-specialization/services/doctor-specialization.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {DoctorSpecialization} from '../../doctor/domain/doctor-specialization';
 import {Observable, of} from 'rxjs';
+import {environment} from '../../../../environments/environment.dev';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorSpecializationService {
+  private url = `${environment.serverUrl}/doctor-specializations`;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
   ) { }
 
   public buildDoctorSpecialization(): FormGroup {
@@ -20,12 +24,24 @@ export class DoctorSpecializationService {
     })
   }
 
-  create(doctorSpecializationToCreate: DoctorSpecialization) : Observable<DoctorSpecialization>{
-    return of(doctorSpecializationToCreate);
+  create(pwzNumber:string, doctorSpecializationToCreate: DoctorSpecialization) : Observable<DoctorSpecialization[]>{
+    const specializations = [doctorSpecializationToCreate];
+    return this.httpClient.post<DoctorSpecialization[]>(
+      `${this.url}/add`,
+      specializations,
+      {
+        params: {
+          pwzNumber: pwzNumber
+        }
+      }
+    );
   }
 
   update(doctorSpecialization: DoctorSpecialization): Observable<DoctorSpecialization> {
-    return of(doctorSpecialization);
+    return this.httpClient.put<DoctorSpecialization>(
+      `${this.url}`,
+      doctorSpecialization
+    );
   }
 
   delete(doctorSpecialization: DoctorSpecialization): Observable<DoctorSpecialization> {
@@ -43,9 +59,22 @@ export class DoctorSpecializationService {
       "Orthopedic Surgeon",
       "Gastroenterologist",
       "Endocrinologist",
-      "Ophthalmologist"
+      "Kardiologia"
     ];
 
-    return of(doctorSpecializations);
+    return this.httpClient.get<string[]>(
+      `${this.url}/specialization-names`
+    );
+  }
+
+  getDoctorSpecializations(pwzNumber: string): Observable<DoctorSpecialization[]> {
+    return this.httpClient.get<DoctorSpecialization[]>(
+      `${this.url}`,
+      {
+        params: {
+          pwzNumber: pwzNumber
+        }
+      }
+    );
   }
 }
